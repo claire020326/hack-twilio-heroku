d20b5a1542faf0910d80cae1ae732f1d9d1d4a13
claire <claire020326@gmail.com>
2013-01-19 01:17:08 -0800

    my sec commit

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-import os
+
 from flask import Flask
-
+import twilio.twiml
+ 
 app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return 'Hello World!'
-
-if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+ 
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond to incoming requests."""
+    resp = twilio.twiml.Response()
+    resp.say("Hello Monkey")
+ 
+    return str(resp)
+ 
+if __name__ == "__main__":
+    app.run(debug=True)
