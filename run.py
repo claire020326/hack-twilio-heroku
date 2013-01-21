9b99b7b9ab23d5a81358f31e13312ea4940e6405
claire <claire020326@gmail.com>
2013-01-21 00:25:45 -0800

    ok

--- run.py
+++ run.py
@@ -22,19 +22,37 @@ def hello_monkey():
     with resp.gather(numDigits=1, action = "/handle-key",method = "POST") as g:
         g.say("To speak to real Claire, press 1. Press any other key to start over.")
     return str(resp)
-@app.route("/handle-key",methods = ['GET','POST'])
+#@app.route("/handle-key",methods = ['GET','POST'])
+#def handle_key():
+#    """Handle key press from a user."""
+#    #Get the digit pressed by the user
+#    digit_pressed = request.values.get('Digits',None)
+#    if digit_pressed =="1":
+#        resp = twiml.Response()
+#        #resp.dial("+13105551212")
+#        resp.say("It's a trap, haha!")
+#        resp.play("http://demo.twilio.com/hellomonkey/monkey.mp3")
+#        return str(resp)
+#    else:
+#        return redirect("/") 
+@app.route("/handle-key", methods=['GET', 'POST'])
 def handle_key():
     """Handle key press from a user."""
-    #Get the digit pressed by the user
-    digit_pressed = request.values.get('Digits',None)
-    if digit_pressed =="1":
+ 
+    # Get the digit pressed by the user
+    digit_pressed = request.values.get('Digits', None)
+    if digit_pressed == "1":
         resp = twiml.Response()
-        #resp.dial("+13105551212")
-        resp.say("It's a trap, haha!")
-        resp.play("http://demo.twilio.com/hellomonkey/monkey.mp3")
+        # Dial (310) 555-1212 - connect that number to the incoming caller.
+        resp.dial("+13105551212")
+        # If the dial fails:
+        resp.say("The call failed, or the remote party hung up. Goodbye.")
+ 
         return str(resp)
+ 
+    # If the caller pressed anything but 1, redirect them to the homepage.
     else:
-        return redirect("/") 
+        return redirect("/")
 if __name__ == "__main__":
      port = int(os.environ.get('PORT',5000)) 
      app.run(debug=True,host='0.0.0.0',port=port)
