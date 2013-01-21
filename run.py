dfabbf5082b7125aa2ba51e1d1be29d17ef8d9b4
claire <claire020326@gmail.com>
2013-01-20 21:49:12 -0800

    changed to junchao lu

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def hello_monkey():
     """Respond to incoming requests."""
     resp = twilio.twiml.Response()
-    resp.say("Thanks for calling. You reach Claire Li but she is not available now. Please call her later!")
+    resp.say("Thanks for calling. You reach Claire Li but she is not available now and she knows you are Junchao Lu. Don't expect her to take your call!")
  
     return str(resp)
  
