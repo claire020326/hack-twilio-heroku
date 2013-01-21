b8a10bb340df018c63fca3d3ab2f1cf8dd76385d
claire <claire020326@gmail.com>
2013-01-21 00:02:26 -0800

    omg

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 
-from flask import Flask
+from flask import Flask,request,redirect
 import twilio.twiml
 import os
  
@@ -35,5 +35,6 @@ def handle_key():
        return redirect("/") 
 if __name__ == "__main__":
      port = int(os.environ.get('PORT',5000)) 
-     app.run(debug=True,host='0.0.0.0',port=port)
+     #app.run(debug=True,host='0.0.0.0',port=port)
+     app.run(debug=True)
 
