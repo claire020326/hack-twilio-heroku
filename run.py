9bb60e176c14937bd69272a7ce25b56e769395c8
claire <claire020326@gmail.com>
2013-01-21 00:57:18 -0800

    re-fixed

--- run.py
+++ run.py
@@ -7,23 +7,23 @@ app = Flask(__name__)
 callers = {
 "+17142670397":"Siyao","+15105178020":"Lisa Li","+15155094420":"Shirly Chen","+16154297319":"Cynthia Feng","+15072029530":"Claire Li",
 } 
-#@app.route("/", methods=['GET', 'POST'])
-#def hello_monkey():
-#    from_number = request.values.get('From',None)
-#    #"""Respond to incoming requests."""
-#    if from_number in callers:
-#	#Greet the caller by name
-#        caller = callers[from_number]
-#    else:
-#	caller = "Slacker"
-#    resp = twilio.twiml.Response()
-#    resp.say("Hi "+caller+" this is Claire Li's private number")
-#    #resp.play("http://demo.twilio.com/hellomonkey/monkey.mp3")
-#    with resp.gather(numDigits=1, action = "/handle-key",method = "POST") as g:
-#        g.say("""To speak to real Claire, press 1.
-#                 Press 2 to record your message.
-#                 Press any other key to start over.""")
-#    return str(resp)
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    from_number = request.values.get('From',None)
+    #"""Respond to incoming requests."""
+    if from_number in callers:
+	#Greet the caller by name
+        caller = callers[from_number]
+    else:
+	caller = "Slacker"
+    resp = twilio.twiml.Response()
+    resp.say("Hi "+caller+" this is Claire Li's private number")
+    #resp.play("http://demo.twilio.com/hellomonkey/monkey.mp3")
+    with resp.gather(numDigits=1, action = "/handle-key",method = "POST") as g:
+        g.say("""To speak to real Claire, press 1.
+                 Press 2 to record your message.
+                 Press any other key to start over.""")
+    return str(resp)
 @app.route("/handle-key",methods = ['GET','POST'])
 def handle_key():
     """Handle key press from a user."""
@@ -37,24 +37,24 @@ def handle_key():
         return str(resp)
     else:
         return redirect("/") 
-@app.route("/handle-key", methods=['GET', 'POST'])
-def handle_key():
-    """Handle key press from a user."""
+#@app.route("/handle-key", methods=['GET', 'POST'])
+#def handle_key():
+ #   """Handle key press from a user."""
  
     # Get the digit pressed by the user
-    digit_pressed = request.values.get('Digits', None)
-    if digit_pressed == "1":
-        resp = twilio.twiml.Response()
-        # Dial (310) 555-1212 - connect that number to the incoming caller.
-        resp.dial("+13105551212")
-        # If the dial fails:
-        resp.say("The call failed, or the remote party hung up. Goodbye.")
+#    digit_pressed = request.values.get('Digits', None)
+ #   if digit_pressed == "1":
+  #      resp = twilio.twiml.Response()
+   #     # Dial (310) 555-1212 - connect that number to the incoming caller.
+    #    resp.dial("+13105551212")
+     #   # If the dial fails:
+      #  resp.say("The call failed, or the remote party hung up. Goodbye.")
  
-        return str(resp)
+       # return str(resp)
  
     # If the caller pressed anything but 1, redirect them to the homepage.
-    else:
-        return redirect("/")
+    #else:
+     #   return redirect("/")
 if __name__ == "__main__":
      port = int(os.environ.get('PORT',5000)) 
      app.run(debug=True,host='0.0.0.0',port=port)
